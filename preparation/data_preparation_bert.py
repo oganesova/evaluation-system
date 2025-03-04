7f803f67fc4557632e1399f514a8e96737d688b2
oganesova <lilush.stepanyan@mail.ru>
2025-03-04 19:44:12 +0400

    Data Preparation

---
+++ preparation/data_preparation_bert.py
@@ -0,0 +1,62 @@
+import logging
+
+import pandas as pd
+from datasets import Dataset
+from transformers import BertTokenizerFast
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+model_name = "bert-base-uncased"
+tokenizer = BertTokenizerFast.from_pretrained(model_name)
+
+def load_dataset(file_path):
+    try:
+        data = pd.read_csv(file_path)
+        data = data.dropna()
+        data = data.drop_duplicates()
+        formal_df = data[['formal']].copy()
+        formal_df['label'] = 1
+        formal_df.rename(columns={'formal': 'text'}, inplace=True)
+        informal_df = data[['informal']].copy()
+        informal_df['label'] = 0
+        informal_df.rename(columns={'informal': 'text'}, inplace=True)
+        df = pd.concat([formal_df, informal_df], ignore_index=True)
+        df = df.sample(frac=1, random_state=42).reset_index(drop=True)
+        logging.info("Dataset loaded and shuffled successfully.")
+        return df
+    except FileNotFoundError:
+        logging.error(f"File not found: {file_path}")
+        return
+
+def convert_frame_to_dataset_format(df):
+    logging.info("Converted DataFrame to Hugging Face Dataset format.")
+    return Dataset.from_pandas(df)
+
+def tokenize_function(example):
+    return tokenizer(example["text"], padding="max_length", truncation=True, max_length=128)
+
+def tokenize_dataset(dataset_format):
+    tokenized_dataset = dataset_format.map(tokenize_function, batched=True)
+    tokenized_dataset = tokenized_dataset.remove_columns(["text"])
+    tokenized_dataset = tokenized_dataset.rename_column("label", "labels")
+    tokenized_dataset.set_format("torch")
+    logging.info("Tokenization complete.")
+    return tokenized_dataset
+
+def save_tokenized_data(tokenized_data):
+    try:
+        tokenized_data.save_to_disk("../dataset/tokenized_dataset_bert")
+        logging.info("Tokenized dataset saved to 'dataset/tokenized_dataset_bert'.")
+    except Exception as e:
+        logging.error(f"An error occurred during data saving: {e}")
+        return
+
+
+
+if __name__ == "__main__":
+    dataset_file_path_big = "../dataset/formal_informal_dataset.csv"
+    dataset_file_path_small = "../dataset/formal_informal_dataset_small.csv"
+    dataset_frame = load_dataset(dataset_file_path_small)
+    dataset_format_ = convert_frame_to_dataset_format(dataset_frame)
+    tokenized = tokenize_dataset(dataset_format_)
+    save_tokenized_data(tokenized)
---
+++ preparation/data_preparation_gpt.py
@@ -0,0 +1,58 @@
+import logging
+import pandas as pd
+from datasets import Dataset
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def load_dataset(file_path):
+    try:
+        data = pd.read_csv(file_path)
+        data = data.dropna()
+        data = data.drop_duplicates()
+        formal_df = data[['formal']].copy()
+        formal_df['label'] = 1
+        formal_df.rename(columns={'formal': 'text'}, inplace=True)
+        informal_df = data[['informal']].copy()
+        informal_df['label'] = 0
+        informal_df.rename(columns={'informal': 'text'}, inplace=True)
+        df = pd.concat([formal_df, informal_df], ignore_index=True)
+        df = df.sample(frac=1, random_state=42).reset_index(drop=True)
+        logging.info("Dataset loaded and shuffled successfully.")
+        return df
+    except FileNotFoundError:
+        logging.error(f"File not found: {file_path}")
+        return None
+
+def prepare_prompts(df):
+    df['prompt'] = df['text'].apply(
+        lambda x: f"""Please evaluate if the following text is formal or informal. 
+        Respond with only one word: 'formal' or 'informal'.
+        
+        Text: {x}
+        
+        Response:"""
+    )
+    return df
+
+def convert_to_dataset(data_frame):
+    dataset = Dataset.from_pandas(data_frame)
+    logging.info("Dataset created successfully.")
+    return dataset
+
+def save_dataset(dataset, output_path):
+    try:
+        dataset.save_to_disk(output_path)
+        logging.info(f"Dataset saved to {output_path}")
+        logging.info(f"Dataset size: {len(dataset)}")
+    except Exception as e:
+        logging.error(f"Error saving dataset: {e}")
+
+if __name__ == "__main__":
+    input_file = "../dataset/formal_informal_dataset_small.csv"
+    output_path = "../dataset/dataset_gpt"
+    df = load_dataset(input_file)
+    df = prepare_prompts(df)
+    dataset = convert_to_dataset(df)
+    save_dataset(dataset, output_path)
+    
+    logging.info("Data preparation completed successfully") 
